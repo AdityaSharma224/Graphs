4dfa27b667c1cff75b9e6bcec8d955e1440527f1
ADITYA SHARMA <sharmaaditya13064@gmail.com>
2022-12-03 11:36:58 +0530

    my first commit

---
+++ adjacency_mat.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+
+    int adj[n+1][m+1];
+    for(int i=0;i<n;i++)
+    {
+        int u,v;
+        cin>>u>>v;
+        adj[u][v]=1;
+        adj[v][u]=1;
+    }
+    return 0;
+}
